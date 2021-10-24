85a79aac56fab0f4967b1fee6352e63117e4617f
Tikva1989 <tabukasis1989@gmail.com>
2021-10-24 11:08:48 +0300

    Automatic configuaration with django_heroku

--- zivasite/zivasite/settings.py
+++ zivasite/zivasite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+django_heroku.settings(locals())
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +132,4 @@ STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
